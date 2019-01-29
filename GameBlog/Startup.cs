2570dc241658c7dc3fb2fb70e117aff99e9295be
alicinacat <alice.casaburi@gmail.com>
2019-01-29 15:34:59 +0100

    Connected to database

--- GameBlog/Startup.cs
+++ GameBlog/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using GameBlog.Models;
 
 namespace GameBlog
 {
@@ -15,6 +17,11 @@ namespace GameBlog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            var conn = "Server=localhost; User ID=sa; Password=CAZZOcazzo123; Database=BlogDB;";
+
+            services.AddDbContext<BlogDBContext>(options => options.UseSqlServer(conn));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
