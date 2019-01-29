c3732352319ba885b1059d1e735c63e98773f04f
alicinacat <alice.casaburi@gmail.com>
2019-01-29 15:02:08 +0100

    Added base MVC folders and EF

---
+++ GameBlog/Models/BlogDBContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GameBlog.Models
+{
+    public partial class BlogDBContext : DbContext
+    {
+        public BlogDBContext()
+        {
+        }
+
+        public BlogDBContext(DbContextOptions<BlogDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost; User ID=sa; Password=CAZZOcazzo123; Database=BlogDB;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.Content)
+                    .IsRequired()
+                    .HasMaxLength(2000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
