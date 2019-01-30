1a74c90c60b58c76568f528de9548e774e83bab5
alicinacat <alice.casaburi@gmail.com>
2019-01-30 10:48:26 +0100

    Updated EF model

---
+++ GameBlog/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameBlog.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            PostCategories = new HashSet<PostCategories>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<PostCategories> PostCategories { get; set; }
+    }
+}
